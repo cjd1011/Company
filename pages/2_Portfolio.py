a0f9bc1db3231a4c569cd762a1bca3c0741109b1
cjd1011 <77751497+cjd1011@users.noreply.github.com>
2022-09-25 16:34:25 -0500

    Update 2_Portfolio.py

--- pages/2_Portfolio.py
+++ pages/2_Portfolio.py
@@ -1,60 +1,115 @@
-#pip install openpyxl
-import pandas as pd #pip install pandas
-import plotly.express as px #pip install plotly-express
-import streamlit as st #pip install streamlit
+import streamlit as st
 
-#import datetime #pip install datetime
-
-#import pickle 
-#from pathlib import Path #pip install pathlib
-
-#import altair as alt #pip install altair
-import plotly.graph_objects as go #pip install plotly
-#import streamlit_authenticator as stauth #pip install streamlit-authenticator
-
-st.title("Investment Portfolio")
-
-st.markdown("##")
-
-df = pd.read_excel('Portafolio Inversion.xlsx')
-
-Activo = st.selectbox(
-        "Seleccione el Activo:",
-        options = df['Asset'].unique(),
-        #default = "USD/COP" #Aqui podría por default dejar un filtro especifico pero vamos a dejarlos todos puestos por default
-    )
-
-df_seleccion = df.query("Asset ==@Activo ")
-
-tab1, tab2 = st.tabs(["📈 Chart", "🗃 Data"])
+st.set_page_config(
+     page_title="BBIS Corp - Portfolio",
+     page_icon=":moneybag:",
+     layout="wide",
+     initial_sidebar_state="expanded",
+     #menu_items={
+         #'Get Help': 'https://www.extremelycoolapp.com/help',
+         #'Report a bug': "https://www.extremelycoolapp.com/bug",
+         #'About': "# This is a header. This is an *extremely* cool app!"
+     #}
+ )
 
 
-tab1.subheader("Asset Chart")
+st.title("Our Business Intelligence Portfolio")
 
-fig1 = go.Figure(data=[go.Candlestick(x=df_seleccion['Date'],
-                open=df_seleccion['Open'],
-                high=df_seleccion['High'],
-                low=df_seleccion['Low'],
-                close=df_seleccion['Close'])])
+st.header("Improve Processes")
 
-fig1.update_xaxes(
-    rangeslider_visible=True,
-    rangeselector=dict(
-        buttons=list([
-            dict(count=1, label="1m", step="month", stepmode="backward"),
-            dict(count=6, label="6m", step="month", stepmode="backward"),
-            dict(count=1, label="YTD", step="year", stepmode="todate"),
-            dict(count=1, label="1y", step="year", stepmode="backward"),
-            dict(step="all")
-        ])
-    )
-)
+col1, col2, col3 = st.columns(3)
 
+with col1:
 
-tab1.write(fig1)
+    st.subheader(" 1. Review processes")
 
-tab2.subheader("Asset Database")
+    st.image("Organice.jpg")
 
-tab2.write(df_seleccion)
+    st.caption("Understanding and reviewing processes")
+    #st.caption("      Reviewing the process")         
+                    
+            
+        
+with col2:       
 
-#st.dataframe(df_seleccion)
+    st.subheader(" 2. Standardize processes")     
+        
+    st.image("Standard3.jpg")
+
+    st.caption("Organizing databases and eliminating duplicate processes") #and improve the quality of your information    
+
+        
+with col3:        
+        
+    st.subheader(" 3. Save and deliver the information")  
+    
+    st.image("Send.jpg")
+
+    st.caption("Deliver the information to the stakeholders ")
+    
+st.header("Data Visualization")    
+
+col4, col5, col6 = st.columns(3)
+
+with col4:
+
+    st.subheader(" 1. Identify the type of report")
+
+    st.image("type.jpg")
+
+    st.caption("Knowing your report needs")
+    #st.caption("      Reviewing the process")       
+    
+    
+with col5:
+
+    st.subheader(" 2. Choose the best visual tool")
+
+    st.image("tool.jpg")
+
+    st.caption("With our knowledge we can identify the best tool for our customers")
+    #st.caption("      Reviewing the process")
+    
+    
+with col6:
+
+    st.subheader(" 3. Create the report")
+
+    st.image("dashboard.jpg")
+
+    st.caption("Showing you the relevant information to take the best decisions")
+    #st.caption("      Reviewing the process")    
+    
+
+st.header("Corporate Finance")
+
+col7, col8, col9 = st.columns(3)
+
+with col7:
+
+    st.subheader(" 1. Company Valuation")
+
+    st.image("valuation.jpg")
+
+    st.caption("Fundamental value of your company")
+    #st.caption("      Reviewing the process") 
+    
+    
+with col8:
+
+    st.subheader(" 1. Investment Opportunities")
+
+    st.image("invest.jpg")
+
+    st.caption("Invest and diversify part of your profit")
+    #st.caption("      Reviewing the process")   
+    
+    
+with col9:
+
+    st.subheader(" 1. Investment Dashboard")
+
+    st.image("dash.jpg")
+
+    st.caption("We will provide you a dashboard to control your investments")
+    #st.caption("      Reviewing the process")
